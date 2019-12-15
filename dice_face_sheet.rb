f0c6d62dbeebf128c72e0145a83b1afc95167929
ced <zuger.cedric@gmail.com>
2019-12-15 19:57:32 +0100

    Working on dice faces alignment.

--- dice_face_sheet.rb
+++ dice_face_sheet.rb
@@ -1,17 +1,17 @@
 require 'RMagick'
 include Magick
 
-final_picture = Image.new( 100 * 12 + 300 , 100 * 8 + 300 ) { self.background_color = 'white' }
+final_picture = Image.new( 100 * 12 , 100 * 8 * 2 ) { self.background_color = 'white' }
 
 dice = []
 
-of1 = ImageList.new('pics/ordre_face1.jpg' )
-of2 = ImageList.new('pics/ordre_face2.jpg' )
-of3 = ImageList.new('pics/ordre_face3.jpg' )
+of1 = ImageList.new('pics/ordre_face1.jpg' ).resize( 0.8 )
+of2 = ImageList.new('pics/ordre_face2.jpg' ).resize( 0.8 )
+of3 = ImageList.new('pics/ordre_face3.jpg' ).resize( 0.8 )
 
-mf1 = ImageList.new('pics/magie_face1.jpg' )
-mf2 = ImageList.new('pics/magie_face2.jpg' )
-mf3 = ImageList.new('pics/magie_face3.jpg' )
+mf1 = ImageList.new('pics/magie_face1.jpg' ).resize( 0.8 )
+mf2 = ImageList.new('pics/magie_face2.jpg' ).resize( 0.8 )
+mf3 = ImageList.new('pics/magie_face3.jpg' ).resize( 0.8 )
 
 def create_dice( dice, faces_set )
   1.upto( 3 ).each{ dice << faces_set[ 0 ] }
@@ -20,32 +20,32 @@ def create_dice( dice, faces_set )
   dice
 end
 
-def round_corners( image )
-  cols = rows = 100
-  radius = 90
-
-  # Set a transparent background: pixels that are transparent will be
-  # discarded from the source image.
-  mask = Image.new(cols, rows) {self.background_color = 'transparent'}
-
-  # Create a white rectangle with rounded corners. This will become the
-  # mask for the area you want to retain in the original image.
-  Draw.new.stroke('none').stroke_width(0).fill('white').
-    roundrectangle(0, 0, cols, rows, radius, radius).
-    draw(mask)
-
-  # Apply the mask and write it out
-  image.composite!(mask, 0, 0, CopyAlphaCompositeOp)
-  image
-end
-
-of1 = round_corners( of1 )
-of2 = round_corners( of2 )
-of3 = round_corners( of3 )
-
-mf1 = round_corners( mf1 )
-mf2 = round_corners( mf2 )
-mf3 = round_corners( mf3 )
+# def round_corners( image )
+#   cols = rows = 100
+#   radius = 90
+#
+#   # Set a transparent background: pixels that are transparent will be
+#   # discarded from the source image.
+#   mask = Image.new(cols, rows) {self.background_color = 'transparent'}
+#
+#   # Create a white rectangle with rounded corners. This will become the
+#   # mask for the area you want to retain in the original image.
+#   Draw.new.stroke('none').stroke_width(0).fill('white').
+#     roundrectangle(0, 0, cols, rows, radius, radius).
+#     draw(mask)
+#
+#   # Apply the mask and write it out
+#   image.composite!(mask, 0, 0, CopyAlphaCompositeOp)
+#   image
+# end
+#
+# of1 = round_corners( of1 )
+# of2 = round_corners( of2 )
+# of3 = round_corners( of3 )
+#
+# mf1 = round_corners( mf1 )
+# mf2 = round_corners( mf2 )
+# mf3 = round_corners( mf3 )
 
 1.upto( 8 ).each do
   dice = create_dice( dice, [ of1, of2, of3 ] )
@@ -63,12 +63,9 @@ row = 0
 until dice.empty?
   face = dice.shift
 
-  # face.alpha(Magick::ActivateAlphaChannel)
-  # f.alpha(Magick::ActivateAlphaChannel)
-
   next unless face
 
-  final_picture.composite!( face, col * 100 + 5, row * 100 + 5, OverCompositeOp )
+  final_picture.composite!( face, col * 100 + 7, row * 100 + 7, OverCompositeOp )
 
   col += 1
 
